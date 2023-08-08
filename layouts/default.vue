275df2e2116593bf155d375eee37ef07df8d967b
Valijon Coder <valijoncoder9085@gmail.com>
2023-08-08 07:48:57 -0700

    remove container

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,35 +1,33 @@
 <template>
   <div>
-    <v-container>
-      <SidebarPage/>
-      <v-btn @click="clickBtn" ref="to-top" id="to-top" dark class="rounded-lg">
-        <v-icon>
-          mdi-chevron-up
-        </v-icon>
-      </v-btn>
-    </v-container>
+    <SidebarPage/>
+    <v-btn @click="clickBtn" ref="to-top" id="to-top" dark class="rounded-lg">
+      <v-icon>
+        mdi-chevron-up
+      </v-icon>
+    </v-btn>
   </div>
 </template>
 
 <script>
 import SidebarPage from "~/components/Sidebar.vue";
 import FooterComponents from "@/components/Footer.vue";
+
 window.addEventListener('scroll', () => {
 
   const toTopBtn = document.getElementById('to-top');
   const scrollY = window.scrollY;
-  if (Math.ceil(scrollY) > 400){
+  if (Math.ceil(scrollY) > 400) {
     toTopBtn.style.opacity = '1';
-  }
-  else {
+  } else {
     toTopBtn.style.opacity = '0'
   }
 })
 export default {
   name: 'DefaultLayout',
   components: {FooterComponents, SidebarPage},
-  methods:{
-    clickBtn(){
+  methods: {
+    clickBtn() {
       window.scrollTo({
         top: 0,
         behavior: 'smooth',
@@ -39,7 +37,7 @@ export default {
 }
 </script>
 <style lang="scss">
-#to-top{
+#to-top {
   position: fixed;
   right: 3%;
   bottom: 3%;
@@ -47,6 +45,7 @@ export default {
   background: linear-gradient(139deg, #0677E1 0%, #8C1FE2 100%);
   transition: all .4s ease;
 }
+
 @media (max-width: 980px) {
   .v-application--wrap {
     padding: 0 10px !important;
