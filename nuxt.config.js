98e8c970a469178266fd8967e97277c0e19dd750
Valijon Coder <valijoncoder9085@gmail.com>
2023-08-08 02:56:47 -0700

    favicon

--- nuxt.config.js
+++ nuxt.config.js
@@ -6,8 +6,8 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    titleTemplate: '%s - programmist',
-    title: 'programmist',
+    titleTemplate: '',
+    title: 'Programmist.UZ',
     htmlAttrs: {
       lang: 'en'
     },
@@ -18,7 +18,7 @@ export default {
       { name: 'format-detection', content: 'telephone=no' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { rel: 'icon', type: 'image/x-icon', href: '/logo-png.jpg' }
     ]
   },
 
